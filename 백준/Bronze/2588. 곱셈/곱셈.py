8126b0a83355ea2f62c49707c13bc74b070f03ab
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2025-03-08 19:25:08 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,17 @@
+n1 = input()
+n2 = input()
+n1 = int(n1)
+n2_0 = int(n2[0])
+n2_1 = int(n2[1])
+n2_2 = int(n2[2])
+
+n3 = n1 * n2_2
+n4 = n1 * n2_1
+n5 = n1 * n2_0
+
+n6 = n3 + 10 * n4 + 100 * n5
+
+print(n3)
+print(n4)
+print(n5)
+print(n6)
