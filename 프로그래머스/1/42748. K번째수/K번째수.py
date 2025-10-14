80661379954953609c54f8b8c0131234e9a29fbd
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2025-10-14 16:36:58 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,18 +1,6 @@
-def prep(elements, array):
-    i = elements[0]
-    j = elements[1]
-    k = elements[2]
-    
-    if len(array) == 1:
-        return array[0]
-    else:
-        prep1 = array[i-1:j]
-        prep2 = sorted(prep1)[k-1]
-    
-    return prep2
-    
 def solution(array, commands):
     answer = []
-    for i in commands:
-        answer.append(prep(i, array))
+    for command in commands:
+        i, j, k = command
+        answer.append(sorted(array[i-1:j])[k-1])
     return answer
