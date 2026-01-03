e464639d2f4efccee2eb1c4dc3fb6e4a678db37f
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2026-01-03 15:29:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.89 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 38 MB, 시간: 74.10 ms
+메모리: 33 MB, 시간: 49.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 16:21:11
+2026년 01월 03일 15:29:13
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,17 @@
-from collections import Counter
-
+# 참가자 명단에서 완주자 명단을 빼고 나면 누가 남는가?
+# dictionary에 participant 이름이 새로 나올 때마다 key추가 후 value 에 1
+# 동명이인일 경우 value +1
 def solution(participant, completion):
-    count_p = Counter(participant)
-    count_c = Counter(completion)
-    diff = count_p - count_c
-    return list(diff.keys())[0]
+    answer = ''
+    count = {}
+    for part in participant:
+        if part not in count.keys():
+            count[part] = 1
+        else:
+            count[part] += 1
+    for com in completion:
+        count[com] -= 1
+    for name, c in count.items():
+        if c != 0:
+            answer += name
+    return answer
