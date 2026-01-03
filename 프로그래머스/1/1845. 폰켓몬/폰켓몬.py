62e8dca5390e2e95267752d98e3212cf90ff6a94
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2026-01-03 16:35:13 +0900

    [level 1] Title: 폰켓몬, Time: 1.36 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,15 @@
-from collections import Counter
+# 딕셔너리 이용해서 각 숫자당 몇 마리가 있는지 확인하기
 def solution(nums):
-    x = Counter(nums)       # 종류별 폰켓몬 수 딕셔너리
-    length = len(nums) // 2      # 가져갈 수 있는 폰켓몬 개수
-    if (len(x.keys()) >= length):
-        answer = length
+    cnt = {}
+    answer = 0
+    for num in nums:
+        if num in cnt:
+            cnt[num] += 1
+        else:
+            cnt[num] = 1
+    get = len(nums)/2   # 가져갈 수 있는 폰켓몬 수
+    if get >= len(cnt.keys()):
+        answer = len(cnt.keys())
     else:
-        answer = len(x.keys())
+        answer = get
     return answer
