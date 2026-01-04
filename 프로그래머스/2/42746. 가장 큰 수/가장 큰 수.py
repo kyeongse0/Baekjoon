332b8d06d669dae43b56aeb782e9eeea18055e40
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2026-01-04 17:15:34 +0900

    [level 2] Title: 가장 큰 수, Time: 62.42 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,11 @@
+# 왜 정렬 카테고리 문제일까?
+
+# 1. 숫자를 문자열로 변환 2. 정렬 기준 설정 3. 실제로 정렬 4. 엣지케이스 고려(0000)
 def solution(numbers):
-    str_nums = list(map(str, numbers))
-    # 각 숫자를 3번 반복해 비교 (최대 숫자가 1000이므로 3자리)
-    str_nums.sort(key=lambda x: x*3, reverse=True)
-    answer = ''.join(str_nums)
-    if answer[0] == '0':
-        return '0'
-    return answer
+    answer = ''
+    numbers = list(map(str, numbers))   # 숫자를 문자열로 변환
+    numbers.sort(key = lambda x :x*3, reverse=True)
+    
+    answer="".join(numbers)
+    
+    return '0' if answer[0] == '0' else answer  # 엣지 케이스 고려
