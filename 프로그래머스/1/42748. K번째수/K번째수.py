f5eed2572070e8727ab018fa6e0f683d1ec0ef06
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2026-01-04 16:06:20 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,7 @@
+# i ~ j 번째까지 자르기 , 정렬하기, k번째 숫자 찾기
+# commands가 2차원 배열
 def solution(array, commands):
     answer = []
     for command in commands:
-        i, j, k = command
-        answer.append(sorted(array[i-1:j])[k-1])
+        answer.append(sorted(array[command[0]-1 : command[1]])[command[2]-1])
     return answer
