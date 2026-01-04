0ebdfc99bf19bc65300ceed9f1f8bb5b8fdab834
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2026-01-04 15:09:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.54 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,9 +1,12 @@
+# 딕셔너리 이용해도 되지 않을까...?
+# 안됨 뒤에 같은 숫자가 나왔을 때 유지가 불가능
+# 스택이냐 큐냐? 직전 숫자와 비교하는 데에는 스택이 Good
 def solution(arr):
     answer = []
     answer.append(arr[0])
-    for i in range(1, len(arr)):
-        if (arr[i-1] != arr[i]):
-            answer.append(arr[i])
+    for i in arr:
+        if answer[-1] == i:
+            pass
         else:
-            continue
+            answer.append(i)
     return answer
