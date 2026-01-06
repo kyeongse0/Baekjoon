718fe0d8490cec67848b64935ddb77053b3967c8
kyeongseo <140937040+kyeongse0@users.noreply.github.com>
2026-01-06 22:43:32 +0900

    [level 2] Title: 타겟 넘버, Time: 449.76 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,16 @@
+# +, - 두 가지 있음
+# 깊이 우선 탐색인 것 같은데? +/값\-
+# 쭉 내려가서 계산된 값을 리스트에 저장 -> 저장된 리스트 내에 target값이 몇 개인지 count
+# 어떻게 구현? for문 안에서 dfs 함수 반복 호출? 
 def solution(numbers, target):
     answer = 0
-    def dfs(depth, x):
-        nonlocal answer
-        if (depth == len(numbers)):
-            if (x == target):
-                answer +=1
+    def dfs(idx, val):
+        nonlocal answer     # nonlocal을 써야하는 이유
+        if idx == len(numbers):     # 깊이 끝까지 같을 때
+            if val == target:       # target 값과 같은지 확인
+                answer +=1          # 같으면 answer +1
             return
-        dfs(depth+1, x + numbers[depth])
-        dfs(depth+1, x - numbers[depth])
-        
-    dfs(0,0)
+        dfs(idx+1, val - numbers[idx])  # 다음 깊이로 -
+        dfs(idx+1, val + numbers[idx])  # 다음 깊이로 +
+    dfs(0,0)    # 0부터 시작
     return answer
